ad9ae0feeff1d1220e4d446e80d9b25a4519b59f
Mehedihasan023 <mh20973@gmail.com>
2023-08-13 03:19:41 +0600

    all category  added

--- src/Shared/LeftNav/LeftNav.jsx
+++ src/Shared/LeftNav/LeftNav.jsx
@@ -1,10 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.log(error))
+
+    }, [])
+
     return (
         <div>
-            <h2>left nav</h2>
+            <h2>All Category</h2>
+         <div className='ps-4'>
+                {
+                    categories.map(category => <p
+                        key={category.id}>
+                        <Link to={`/category/${category.id}`} className='text-decoration-none text-black'>{category.name}</Link>
+                    </p>)
+                }
+         </div>
         </div>
     );
 };
